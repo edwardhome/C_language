9b707defe473ceb585d450ba0f3d7358469bf200
edwardhome <edward61221@gmail.com>
2021-10-26 15:42:10 +0800

    新增basic資料夾與其內容，說明C語言學習筆記

---
+++ basic/datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("型態\t\t大小（bytes）\n");
+    printf("short\t\t%lu\n", sizeof(short));
+    printf("int\t\t%lu\n", sizeof(int));
+    printf("long\t\t%lu\n", sizeof(long));
+    printf("float\t\t%lu\n", sizeof(float));
+    printf("double\t\t%lu\n", sizeof(double));
+    printf("long double\t%lu\n", sizeof(long double));
+    printf("char\t\t%lu\n", sizeof(char));
+    return 0;
+}
